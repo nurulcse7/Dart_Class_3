766e30c7e69f0febbac9dd87e58fe212e69d283a
Nurul Islam <nurul.cse7@gmail.com>
2025-03-14 00:51:50 +0600

    final const added

---
+++ bin/final_const.dart
@@ -0,0 +1,11 @@
+// import 'package:dart_class_3/dart_class_3.dart' as dart_class_3;
+
+void main(){
+
+  final DateTime nowDateTime; //runtime time
+  const double pi = 3.1416; // compile  time;
+
+  nowDateTime = DateTime.now();
+
+  print(nowDateTime);
+}
